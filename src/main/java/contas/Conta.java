133d86325b2c810b2cbad56cddc10d6a3210ca75
AgnoDev <agnodev87@gmail.com>
2023-04-27 16:11:50 -0300

    implementa metodos 'sacar', 'depositar' e 'transferir'

--- src/main/java/contas/Conta.java
+++ src/main/java/contas/Conta.java
@@ -6,7 +6,30 @@ public class Conta {
     String titular;
     double saldo;
 
-   public void depositar(double valor) {
-       this.saldo += valor;
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque realizado com sucesso!");
+            return true;
+        } else {
+            System.out.println("Saldo insuficiente");
+            return false;
+        }
+    }
+
+    public boolean transferir(double valor, Conta contaDestino){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            contaDestino.depositar(valor);
+            System.out.println("Transferência realizada com sucesso");
+            return true;
+        }else{
+            System.out.println("Saldo insuficiente");
+            return false;
+        }
     }
 }
