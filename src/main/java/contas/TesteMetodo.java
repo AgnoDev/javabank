3de56163b740c6d24ff7504856aef2baf60303ad
AgnoDev <agnodev87@gmail.com>
2023-04-27 16:11:12 -0300

    testa todos os metodos da conta

--- src/main/java/contas/TesteMetodo.java
+++ src/main/java/contas/TesteMetodo.java
@@ -7,7 +7,22 @@ public class TesteMetodo {
         Conta conta1 = new Conta();
         conta1.saldo = 100;
         conta1.depositar(50);
-        System.out.println(conta1.saldo);
+        System.out.println("Saldo conta1: " + conta1.saldo);
 
+        conta1.sacar(50);
+        System.out.println("Saldo conta1: " + conta1.saldo);
+
+        conta1.sacar(250);
+        System.out.println("Saldo atual conta1: " + conta1.saldo);
+
+        Conta conta2 = new Conta();
+        conta2.saldo = 100;
+        conta2.transferir(50, conta2);
+        System.out.println("Saldo conta2: " + conta2.saldo);
+        System.out.println("Saldo conta1: " + conta1.saldo);
+
+        conta2.transferir(200, conta2);
+        System.out.println("Saldo conta2: " + conta2.saldo);
+        System.out.println("Saldo conta1: " + conta1.saldo);
     }
 }
